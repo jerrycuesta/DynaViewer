6e12e3b3ed324f8985d985af39240d73f1cf5ac7
jerrycuesta <jerry_burch@my.cuesta.edu>
2014-05-14 20:59:06 -0700

    Added settings menu

--- app/src/main/java/com/jerry/dynaviewer/app/CardListActivity.java
+++ app/src/main/java/com/jerry/dynaviewer/app/CardListActivity.java
@@ -3,10 +3,14 @@ package com.jerry.dynaviewer.app;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class CardListActivity extends Activity
         implements CardListFragment.Callbacks {
+
+    boolean useLocalContent = true;
 
     CardListFragment listFragment;
     CardDetailFragment detailFragment;
@@ -45,14 +49,26 @@ public class CardListActivity extends Activity
         detailFragment.setCard(id);
     }
 
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-//            Bundle arguments = new Bundle();
-//            arguments.putString(CardDetailFragment.ARG_ITEM_ID, id);
-//            detailFragment = new CardDetailFragment();
-//            detailFragment.setArguments(arguments);
-//            getFragmentManager().beginTransaction()
-//                    .replace(R.id.card_detail_container, detailFragment)
-//                    .commit();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_local_content) {
+            if (useLocalContent)
+                item.setTitle("Use Remote Content");
+            else
+                item.setTitle("Use Local Content");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
